017c0553c343d321e9b8c52d3ad066fc3dd9701b
Z-D-J <1224649136@qq.com>
2021-09-10 19:25:57 +0800

    基本计算器初步

--- 技术笔记/LeetCode/Code/src/main/java/string/BasicCalculatorII227.java
+++ 技术笔记/LeetCode/Code/src/main/java/string/BasicCalculatorII227.java
@@ -5,21 +5,71 @@ import java.util.Stack;
 
 public class BasicCalculatorII227 {
 
+    ArrayList<Integer> tempNums = new ArrayList<>();
+
     public int calculate(String s) {
 
         char[] s1 = s.toCharArray();
-        ArrayList<Integer> nums = new ArrayList<>();
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> nums = new Stack<>();
+        Stack<Character> ops = new Stack<>();
+        int res = 0;
 
-        for(char c : s1) {
-            if(isNum(c)) {
-                nums.add(c - '0');
-            } else if(isSpace(c)) {
+        for(int i = 0; i < s1.length;) {
+            char c = s1[i];
+
+            i = getNums(s1, i);
+
+            if(isSpace(c)) {
+                i++;
                 continue;
             } else if(isOp(c)) {
+                nums.add(getNum(tempNums));
 
+                if(ops.isEmpty()) {
+                    ops.add(c);
+                    i++;
+                    continue;
+                }
+               char temp = ops.peek();
+               if(temp == '+' || temp == '-' ) {
+                   i++;
+                   ops.add(c);
+                   continue;
+               }
+               if (c == '*' ) {
+                   int num1 = nums.pop();
+                   i = getNums(s1, i);
+                   int num2 = getNum(tempNums);
+                   int num3;
+                   num3 = num2 * num1;
+                   nums.push(num3);
+               }
+
+               if(c == '/') {
+                   int num1 = nums.pop();
+                   i = getNums(s1, i);
+                   int num2 = getNum(tempNums);
+                   int num3;
+                   num3 = num2 * num1;
+                   nums.push(num3);
+               }
             }
         }
+
+        while(!nums.isEmpty() && !ops.isEmpty()) {
+            int num1 = nums.pop();
+            int num2 = nums.pop();
+            int num3;
+            char op = ops.pop();
+            if(op == '+') {
+                num3 = num1 + num2;
+                res += num3;
+            } else {
+                num3 = num2 - num1;
+                res += num3;
+            }
+        }
+        return res;
     }
 
     public boolean isNum(char c) {
@@ -53,7 +103,22 @@ public class BasicCalculatorII227 {
             res += num * Math.pow(10, length - i - 1);
             i++;
         }
+        //清空集合
+        list.clear();
 
         return res;
+    }
+
+    public int getNums(char[] s, int start) {
+        int i;
+        for(i = start; i < s.length; i++) {
+            char c = s[i];
+            if(isNum(c)) {
+                tempNums.add(c - '0');
+            } else {
+                break;
+            }
+        }
+        return i;
     }
 }
