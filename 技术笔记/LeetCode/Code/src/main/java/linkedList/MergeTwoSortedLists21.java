9d53795689b0f26e5e5bba526275a02183383284
Z-D-J <1224649136@qq.com>
2021-09-14 15:24:25 +0800

    创建文本读写

--- 技术笔记/LeetCode/Code/src/main/java/linkedList/MergeTwoSortedLists21.java
+++ 技术笔记/LeetCode/Code/src/main/java/linkedList/MergeTwoSortedLists21.java
@@ -3,16 +3,16 @@ package linkedList;
 public class MergeTwoSortedLists21 {
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-
+        
         ListNode head = new ListNode();
         ListNode cur = head;
         ListNode cur1 = l1;
         ListNode cur2 = l2;
         while(cur1 != null || cur2 != null) {
             int val1;
-            int val2;
+            int val2
             if(cur1 != null) {
-                
+
             }
         }
     }
---
+++ 技术笔记/Python文本读写.md
@@ -0,0 +1,6 @@
+---
+title: Python文本读写
+date: 2021-09-14 08:01:19
+tags: [Python, 大数据]
+categories: 技术笔记
+---
