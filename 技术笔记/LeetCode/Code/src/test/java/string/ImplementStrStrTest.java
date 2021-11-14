571112c5126f6ba9eb6105bcea498f95bbe1a16a
zestaken <1224649136@qq.com>
2021-11-14 11:50:11 +0800

    KMP算法完成

--- 技术笔记/LeetCode/Code/src/test/java/string/ImplementStrStrTest.java
+++ 技术笔记/LeetCode/Code/src/test/java/string/ImplementStrStrTest.java
@@ -14,10 +14,22 @@ public class ImplementStrStrTest {
         String haystack2 = "aaa", needle2 = "a";
         int e2 = 0;
 
+        String haystack3 = "aaa", needle3 = "aaa";
+        int e3 = 0;
+        
+        String haystack4 = "aabaaabaaac", needle4 = "aabaaac";
+        int e4 = 4;
+        
         int res1 = implementStrStr.strStr(haystack1, needle1);
         assert res1 == e1 : res1;
 
         int res2 = implementStrStr.strStr(haystack2, needle2);
         assert res2 == e2 : res2;
+
+        int res3 = implementStrStr.strStr(haystack3, needle3);
+        assert res3 == e3 : res3;
+
+        int res4 = implementStrStr.strStr(haystack4, needle4);
+        assert res4 == e4 : res4;
     }
 }
